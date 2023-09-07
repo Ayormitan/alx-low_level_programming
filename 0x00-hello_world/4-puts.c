9a3f4b2da368f26b9c38ad715c237753dc580659
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-07 06:50:38 -0700

    Printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - entry point
+ *
+ * Return: Always success
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
