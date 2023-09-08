13679ed97139ba32bdbc62c21485d093e577d605
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-08 15:17:44 -0700

    Print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	char ch = 'a';
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	while (ch <= 'f')
+        {
+                putchar(ch);
+		ch++;
+        }
+	putchar('\n');
+	return (0);
+}
