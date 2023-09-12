ba3b762391025a93cab9ff16d8b7a67ac763d007
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-12 09:56:12 -0700

    Printing letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _isalpha - function to print letters
  *
- *
+ * Return: On succes  1, else 0
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
