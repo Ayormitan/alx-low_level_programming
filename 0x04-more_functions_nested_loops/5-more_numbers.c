c4fffc0e697e43ba4d1ce091d5f13d043634b004
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-14 12:13:10 -0700

    Printing nmbers in 10 places

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,20 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	i = 0;
-	while (i < 10)
+	i = 1;
+	while (i <= 10)
 	{
 		j = 0;
 		while (j <= 14)
 		{
-			if (j <= 10)
+			if (j >= 10)
 			{
-				_putchar('0' + i);
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 			}
-				else
-				{
-					_putchar('0' + j);
+			else
+			{
+				_putchar(j + '0');
 			}
 			j++;
 		}
