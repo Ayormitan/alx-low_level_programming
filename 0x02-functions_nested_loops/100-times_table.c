bea2d4408d48fd470c5cf1befbeb882b3548d70e
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-17 14:34:01 -0700

    priting time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,21 +9,37 @@ void print_times_table(int n)
 	int rows;
 	int columbs;
 
-	if (n > 15 || n < 0)
-	{
-		return;
-	}
-	for (rows = 0; rows <= n; rows++)
-	{
-		for (columbs = 0; columbs <= n; columbs++)
-		{
-			_putchar('0' + rows * columbs);
-		if (columbs < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		}
-		putchar('\n');
-	}
+	    if (n > 15 || n < 0)
+        {
+                return;
+        }
+        for (rows = 0; rows <= n; rows++)
+        {
+                for (columbs = 0; columbs <= n; columbs++)
+                {
+                        int result = rows * columbs;
+
+                        if (result < 10)
+                        {
+                        _putchar(result + '0');
+                        }
+                        else if (result < 100)
+                        {
+                                _putchar(result / 10 + '0');
+                                _putchar(result % 10 + '0');
+                        }
+                        else
+                        {
+                                _putchar(result / 100 + '0');
+                                _putchar((result / 100) % 10 + '0');
+                                _putchar(result % 10 + '0');
+                        }
+                if (columbs < n)
+                {
+                        _putchar(',');
+                        _putchar(' ');
+                }
+                }
+                putchar('\n');
+        }
 }
