4a4a71a27d5118d1dbff8eba993d42019fb86957
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-17 14:47:13 -0700

    Prints multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,48 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * print_time_table - printing time table function
  *
- *
+ *@n: Value to check
+ *Return: Alway success
  */
 void print_times_table(int n)
 {
 	int rows;
 	int columbs;
+	int result;
 
-	    if (n > 15 || n < 0)
-        {
-                return;
-        }
-        for (rows = 0; rows <= n; rows++)
-        {
-                for (columbs = 0; columbs <= n; columbs++)
-                {
-                        int result = rows * columbs;
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (rows = 0; rows <= n; rows++)
+	{
+		for (columbs = 0; columbs <= n; columbs++)
+		{
+			result = rows * columbs;
 
-                        if (result < 10)
-                        {
-                        _putchar(result + '0');
-                        }
-                        else if (result < 100)
-                        {
-                                _putchar(result / 10 + '0');
-                                _putchar(result % 10 + '0');
-                        }
-                        else
-                        {
-                                _putchar(result / 100 + '0');
-                                _putchar((result / 100) % 10 + '0');
-                                _putchar(result % 10 + '0');
-                        }
-                if (columbs < n)
-                {
-                        _putchar(',');
-                        _putchar(' ');
-                }
-                }
-                putchar('\n');
-        }
+			if (result < 10)
+			{
+			_putchar(result + '0');
+			}
+			else if (result < 100)
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result / 100 + '0');
+				_putchar((result / 100) % 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			if (columbs < n)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
