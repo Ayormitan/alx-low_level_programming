e1f6ecb10d58b9a89fe246bb31ab7d5fc8fbc34e
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-18 10:09:35 -0700

    Prints strings

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - code test
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
