7e70280db3d9bfdfbf046668a6ae3f647286a960
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-18 07:32:18 -0700

    swaps digit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - function to swap digits
+ *
+ * @a: first digit
+ * @b: Second digit
+ * Return: Always success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
