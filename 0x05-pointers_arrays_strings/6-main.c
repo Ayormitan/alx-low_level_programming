ac9d6ad8ad9be73f412e0bf3cfdf908bfe67199f
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-19 00:58:23 -0700

    Prints everyother character

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - code entry point
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "string.h"
+/**
+ * puts2 - function to skip string
+ *
+ * @str: String to skip
+ * Return: Always success
+ */
+void puts2(char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (i < strlen(str))
+	{
+		_putchar(str[i]);
+		i + = 2;
+	}
+	_putchar('\n');
+}
