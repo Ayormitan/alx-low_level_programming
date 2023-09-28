843373010fa909aa8e24f603364cee2eab7f6304
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-28 07:13:48 -0700

    Test strlen_recursive funct

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Code entry point
+ *
+ *Return: Always success
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("this is code");
+	printf("%d\n", n);
+	return (0);
+}
