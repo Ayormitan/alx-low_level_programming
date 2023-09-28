a2549bea27347b941cfb1e1c5c453c6f19ab6676
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-28 05:45:33 -0700

    Func rev string recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,16 @@
 #include "main.h"
+/**
+ * _printrev_recursion - function prints strings recursively
+ *
+ * @s: Pointer to string to call
+ * Return: Always success
+ */
 void _printrev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_printrev_recursion(s + 1);
+		_printrev_recursion(s + 1);/** function calls itself using pointer*/
+		/** Arithmetic before strings is outputed*/
 		_putchar(*s);
 	}
 	else
