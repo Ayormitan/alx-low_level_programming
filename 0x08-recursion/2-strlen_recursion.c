4e9aa86c1762c556d6a6709e0ced9f4f940dd983
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-28 07:19:21 -0700

    Recursively print strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0')/** base case exit at this point*/
 	{
 		return (0);
 	}
 	else
 	{
-		return (1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));/** Return current*/
+		/** string lenght 1 then and with pointer arithmetic to get strlen*/
 	}
 }
