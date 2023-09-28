1f4b455a85f643fc0309beb4c5f230c5625f364e
Ayormitan <kolawoleabaz97@gmail.com>
2023-09-28 05:51:39 -0700

    Funct rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,11 +5,11 @@
  * @s: Pointer to string to call
  * Return: Always success
  */
-void _printrev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_printrev_recursion(s + 1);/** function calls itself using pointer*/
+		_print_rev_recursion(s + 1);/** function calls itself using pointer*/
 		/** Arithmetic before strings is outputed*/
 		_putchar(*s);
 	}
