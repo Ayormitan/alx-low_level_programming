3ff5e2096fa12144f3606f28355f679fec3eff34
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-08 12:14:35 -0700

    Function concatenates string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,30 +3,38 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * string_nconcat - Function concatenates string
  *
- *
- *
+ * @s1: pointer to first string
+ * @s2: Pointer to second string
+ * @n: Size of string
+ *Return: Always success
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-	int i;
-	char *ptr = malloc(sizeof(char) * n);
-	n = len1 + len2 + 1;
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+	unsigned int i;
+	unsigned int j;
+	char *ptr;
 
-	if (n == NULL)
+	if (n >= len1 + len2)
 	{
-		return (-1);
+		n = len1 + len2;
 	}
-	for (i = 0; i < len1; i++)
+	ptr =  malloc(sizeof(char) * (n + 1));
+	if (ptr == NULL)
+	{
+		exit(1);
+	}
+	for (i = 0; i < len1 && i < n; i++)
 	{
 		ptr[i] = s1[i];
 	}
-	for (i = 0; i < len2; i++)
+	for (j = 0; i < n && s2[j] != '\0'; i++, j++)
 	{
-		s[len1 + 1] = s2[i];
-		ptr[n - 1] = '\0';
+		ptr[i] = s2[j];
 	}
+	ptr[i] = '\0';
 	return (ptr);
 }
