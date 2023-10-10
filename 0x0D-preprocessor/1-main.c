0cddb0f7c9a8a3612e0657589f76ff824300b714
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-10 07:08:15 -0700

    Test 1-pi.h func

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,17 @@
+#include "1-pi.h"
+#include <stdio.h>
+/**
+ * main - check code
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 34;
+	b = PI * a * a;
+	printf("%d\n", b);
+	return (0);
+}
