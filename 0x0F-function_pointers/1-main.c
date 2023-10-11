8d43826837427d91f2c9b8ffb2e51c652e02dfa9
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-11 06:36:58 -0700

    Test array_iterator func and output array elements

---
+++ 0x0F-function_pointers/1-main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointer.h"
+int printnum(int ele)
+{
+	printf("%d\n", ele);
+	return(0);
+}
+/**
+ * print_hexnum - Function prints hexadecimal numbers
+ *
+ *@ele: Elemnt to print
+ */
+int print_hexnum(int ele)
+{
+	printf("0x%x\n", ele);
+	return (0);
+}
+/**
+ * main - Test function
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	int array[5] = {3, 4, 23, 67, 894};
+
+	array_iterator(array, 5, &printnum);
+	array_iterator(array, 5, &print_hexnum);
+	return (0);
+}
