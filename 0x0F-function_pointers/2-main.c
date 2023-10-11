bb9bd35d623998fee03850e2303c5bd44bd1fe65
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-11 08:48:36 -0700

    Function search integers

---
+++ 0x0F-function_pointers/2-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointer.h"
+int _is98(int ele)
+{
+	return (ele == 98);
+}
+int _all_positive(int ele)
+{
+	return (ele > 0);
+}
+int _abs_98(int ele)
+{
+	return (ele == 98 || -ele == 98);
+}
+int main(void)
+{
+	int array[6] = {-2, 0, 980, -34, 1003, 43};
+	int index;
+
+	index = int_index(array, 6, _is98);
+	printf("%d\n", index);
+	index = int_index(array, 6, _all_positive);
+	printf("%d\n", index);
+	index = int_index(array, 6, _abs_98);
+	printf("%d\n", index);
+	return (0);
+}
