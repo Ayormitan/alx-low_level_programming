dcb38721bcf61beb70cc8ddb7d9806e63ba3933b
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-11 06:37:54 -0700

    Function iterates array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointer.h"
+/**
+ * array_iterator - function name to print arrays
+ *
+ * @array: Elemtents of the array to print
+ * @size: Array size
+ * @action: Pointer to function that will be used in printing the array
+ * Return: ALway success
+ */
+void array_iterator(int *array, size_t size, int (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
