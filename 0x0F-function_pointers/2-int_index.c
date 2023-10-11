98bc32299b1eb7a91442186c63f97b6a0ef30e8b
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-11 08:49:51 -0700

    Function checks index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointer.h"
+/**
+ * int_index - Function checks checks index to return digit
+ * @array: Pointer to array to check
+ * @size: Size of the array
+ * @cmp: pointer to function used to compare value
+ * Return: ALways succes
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+		return (-1);
+}
