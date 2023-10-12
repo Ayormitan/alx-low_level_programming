3aec060c1a782e543fa1e5597db8ef757f755e71
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-12 03:25:21 -0700

    Function sums all variadic arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,6 +15,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);/** takes list of arg and last number of arg*/
 	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);/**sum the list of avo:wq*/
