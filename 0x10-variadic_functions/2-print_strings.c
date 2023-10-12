f29a7c7b34d352b039880f1d1ed5470c8a0d3969
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-12 09:14:03 -0700

    Prints strings with seperators

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - Variadic function to print strings
+ *
+ * @separator: Pointers to seperator that will be printed within numbers
+ * @n: Preceding or fixed arguments arguments before variadic arg list
+ * Return: Always success
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *strings;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		strings = va_arg(list, char *);/** retreives the next arguments from the list*/
+	/** also takes pointer to string as type*/
+			if (strings == NULL)
+			{
+				printf("(nil)");
+			}
+				else
+				{
+				printf("%s", strings);
+				}
+				if (i < n - 1 && separator != NULL)/** check if the last char*/
+					/*is not reached and not null*/
+				{
+					printf("%s", separator);
+				}
+	}
+	printf("\n");
+	va_end(list);
+}
