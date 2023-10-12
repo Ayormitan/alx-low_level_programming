5634ebc2b2ee07174b8e8d54bd4870e74518afb3
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-12 03:16:08 -0700

    Variadic function sums all arg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function sums variadic function
+ *
+ * @n: Preceding last numbers in the arguments
+ * Return: Sum of variadic number
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list; /**declares list of argumnent*/
+	unsigned int i;
+	int sum;
+
+	va_start(list, n);/** takes list of arg and last number of arg*/
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);/**sum the list of avo:wq*/
+	/*ailable arguments with int type*/
+	}
+	va_end(list);/** end access to the list of arguments*/
+	return (sum);
+}
