50fbe1f0d5c42cfa5d95f4324e5f83e3ecd36738
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-13 00:47:09 -0700

    Function pointer iterates array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * array_iterator - function name to print arrays
@@ -12,6 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+	{
+		exit(0);
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
