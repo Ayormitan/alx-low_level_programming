e3c1011c7498dd653fa10f1fa6bb04b616dd8c32
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-13 05:04:08 -0700

    Header file with struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct name for my dog data
+ *@name: pointer to do name of type char
+ *@age: Pointer to my dog age with float type
+ *@owner: Pointer to the owner with char type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
