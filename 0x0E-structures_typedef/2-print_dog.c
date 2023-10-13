546c20af4597c940c2ea330bfb76199164b37ae9
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-13 06:56:19 -0700

    Function will print all element in struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *print_dog - Function printa all struct dog elements
+ *
+ *@d: Pointer to the struct element
+ *Return: Always succes
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		exit(0);
+	}
+	if (d->name == NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%s\n", d->name);
+	}
+	if (d->age < 0)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("(nil)\n");
+	}
+	{
+		printf("%s\n", d->owner);
+	}
+}
