41205c4e067d54e6317acefd7d36781ed0317cdf
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-13 07:05:58 -0700

    Function will print all element in struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("%s\n", d->name);
+		printf("Name: %s\n", d->name);
 	}
 	if (d->age < 0)
 	{
@@ -27,13 +27,13 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("%f\n", d->age);
+		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
 		printf("(nil)\n");
 	}
 	{
-		printf("%s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
