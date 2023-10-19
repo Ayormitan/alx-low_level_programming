4e7d8cc93b8924205569d396a342b3a2c61f5e1a
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-19 10:58:14 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+int main(void)
+{
+	list_t *head;
+	head = NULL;
+	add_node_end(&head, "Bob");
+	add_node_end(&head, "&");
+	free_list(head);
+	head = NULL;
+	return (0);
+}
