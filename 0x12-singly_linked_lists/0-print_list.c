be504af29064c0c90d9d3d9cead759180adb6edd
Ayormitan <kolawoleabaz97@gmail.com>
2023-10-19 02:46:02 -0700

    Function prints list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Print elements in a list
+ *
+ *@h: Pointer to elements in the list
+ *Return: ALways elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+		printf("[0] (nill)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+	h = h->next;
+	count++;
+	}
+	return (count);
+}
