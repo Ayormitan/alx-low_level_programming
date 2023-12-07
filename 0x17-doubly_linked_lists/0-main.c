568678a2c54c080a5af6acfad318229c5ac1d392
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-07 04:08:36 -0800

    Test function for 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - entry point to check code
+ *
+ * Return: Always EXIT_SUCCESS or 0 indicating success
+ */
+int main(void)
+{
+	dlistint_t *head;
+	dlistint_t *new;
+	dlistint_t hello = {8, NULL, NULL}; /** this variable will store struct members for int, prev and next**/
+	size_t n;
+
+	head = &hello; /** hello is the first node in the list of which head pointer is pointing to it**/
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		printf("MALLOC FAIL");
+		return (EXIT_FAILURE);
+	}
+	new->n = 9; /**set data of the newnod created to 9**/
+	head->prev = new; /** the original head which is first node hello points to the new node**/
+	new->next = head; /**the next new node point to the original head nod agian**/
+	new->prev = NULL;
+	head = new;
+
+	n = print_dlistint(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (EXIT_SUCCESS);
+}
