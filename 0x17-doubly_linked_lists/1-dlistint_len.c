f9e9ce917e3ebe9cb48034a7717884ab42494fdf
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-07 06:12:09 -0800

    Prints the element in a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * dlistint_len - prints elements in a list
+ *
+ * @h: pointer to the list element
+ * Return: Alway list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
 	const dlistint_t *curr = h;
+
 	while (curr != NULL)
 	{
 		count++;
