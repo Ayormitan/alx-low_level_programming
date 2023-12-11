8d89beee67b81f24336ea951ae51de2d7ff61b21
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-11 11:42:57 -0800

    script that creates a dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
-#!bin/bash
-gcc -wall -wextra -werror -pedantic -c -FPIC *.c
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -FPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY=.:$LD_LIBRARY_PATH
